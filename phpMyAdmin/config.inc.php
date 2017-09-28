6bb06d75be833ae227aba2cccf3312c5f1d1c409
Robert Claus <robertclaus@gmail.com>
2017-09-27 23:19:32 -0500

    try mysql as server

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -28,8 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
-$cfg['Servers'][$i]['socket'] = '/var/lib/mysql/mysql.sock';
+$cfg['Servers'][$i]['host'] = 'mysql';
+/*$cfg['Servers'][$i]['socket'] = '/var/lib/mysql/mysql.sock';*/
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
