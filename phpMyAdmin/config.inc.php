d9910519950a1d6a3de4910c5c86fa4fc1b86679
Robert Claus <robertclaus@gmail.com>
2017-09-27 21:52:16 -0500

    change config

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '1{dd0`<Q),5XP_:R9UK%%8\"EEcyH#{o'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
