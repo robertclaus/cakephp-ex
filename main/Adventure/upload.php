3185b2c10134955ac21de1743fad8a3f91a5a7f9
Robert Claus <robertclaus@gmail.com>
2017-11-05 20:58:30 -0600

    Update upload.php to include section folders and create folders dynamically.

--- main/Adventure/upload.php
+++ main/Adventure/upload.php
@@ -7,11 +7,21 @@ if (!empty($_FILES)) {
      
     $tempFile = $_FILES['file']['tmp_name'];          //3             
       
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+    $targetPathFull = dirname( __FILE__ ) . $ds. $storeFolder . $ds . 'Full' . $ds . $_POST['folderName'] . $ds;  //4
+    $targetPathSmall = dirname( __FILE__ ) . $ds. $storeFolder . $ds . 'Small' . $ds . $_POST['folderName'] . $ds;  //4
+    
+	$targetFileFull =  $targetPathFull. $_POST['renameFile'];  //5
+	$targetFileSmall =  $targetPathSmall. $_POST['renameFile'];  //5
  
-    move_uploaded_file($tempFile,$targetFile); //6
-     
+	if (!file_exists($targetPathFull)) {
+		mkdir($targetPathFull, 0777, true);
+	}
+	
+	if (!file_exists($targetPathSmall)) {
+		mkdir($targetPathSmall, 0777, true);
+	}
+ 
+    move_uploaded_file($tempFile,$targetFileFull); //6
+    move_uploaded_file($tempFile,$targetFileSmall); //6
 }
 ?>
